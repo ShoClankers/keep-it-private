f948822257430f93009275b52923b0cf4be58772
Thato <Nkadimeng.Thato@icloud.com>
2026-04-05 12:21:02 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+class myClass:
+
+    
+    __privateVar = 27;
+
+    def __privateMethod(self):
+        print("I'm inside class myClass")
+
+    def hello(self):
+        print("Private Variable value: ",myClass.__privateVar)
+
+foo = myClass()
+foo.hello()
+foo.privateMethod()
